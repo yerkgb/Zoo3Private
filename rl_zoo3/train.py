3885a88a2dc94874648073d0981014da9e90faf5
Yernar Kubegenov <yernar.kubegenov@magna.com>
2024-05-23 11:29:39 +0200

    some small changes in hyper param file and in train.py file set variable to default value to make the run easier...

--- rl_zoo3/train.py
+++ rl_zoo3/train.py
@@ -23,7 +23,7 @@ def train() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument("--algo", help="RL Algorithm", default="a2c", type=str, required=False, choices=list(ALGOS.keys()))
     parser.add_argument("--env", type=str, default="antenna4x4-v1", help="environment ID")
-    parser.add_argument("-tb", "--tensorboard-log", help="Tensorboard log dir", default="", type=str)
+    parser.add_argument("-tb", "--tensorboard-log", help="Tensorboard log dir", default="TensorBoardLog", type=str)
     parser.add_argument("-i", "--trained-agent", help="Path to a pretrained agent to continue training", default="", type=str)
     parser.add_argument(
         "--truncate-last-trajectory",
