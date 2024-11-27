e23a3d65bfaf17315a011fee580395afabf3f0d9
Yernar Schweiny <yernar.kubegenov@magna.com>
2024-11-27 15:13:04 +0100

    hyperparameter changes/ import env cleaned up/ train scipt changes

--- rl_zoo3/import_envs.py
+++ rl_zoo3/import_envs.py
@@ -28,23 +28,6 @@ def create_normalized_env(env_id: str) -> Callable[[Optional[str]], gym.Env]:
 
 
 try:
-    from env.antennaEnv import AntennaPlacementEnv
-except ImportError:
-    AntennaPlacementEnv = None
-    print("Custom Antenna Environment failed to import")
-
-register(
-    # unique identifier for the env `name-version`
-    id="antenna4x4-v1",
-    # path to the class for creating the env
-    # Note: entry_point also accept a class as input (and not only a string)
-    entry_point=AntennaPlacementEnv,
-    # Max number of steps per episode, using a `TimeLimitWrapper`
-    max_episode_steps=25,
-    )
-del AntennaPlacementEnv
-
-try:
     from env.antennaEnv_V2 import AntennaPlacementEnv
 except ImportError:
     AntennaPlacementEnv = None
@@ -52,7 +35,7 @@ except ImportError:
 
 register(
     # unique identifier for the env `name-version`
-    id="antenna4x4-v2",
+    id="antenna3x4-v2.0",
     # path to the class for creating the env
     # Note: entry_point also accept a class as input (and not only a string)
     entry_point=AntennaPlacementEnv,
@@ -67,21 +50,28 @@ except ImportError:
     AntennaPlacementEnv = None
     print("Custom Antenna Environment failed to import")
 
-
+#### antenna4x4-v1.2
 register(
     id="antenna4x4-v1.2",
     entry_point=AntennaPlacementEnv,
     max_episode_steps=25,
 )
 
+#### antenna3x4-v1.2
 register(
-    id="antenna3x4-v1_1",
+    id="antenna3x4-v1.2",
     entry_point=AntennaPlacementEnv,
     max_episode_steps=25,
 )
 
-
 #### antenna3x3-v1.2
+register(
+    id="antenna3x3-v1_2",
+    entry_point=AntennaPlacementEnv,
+    max_episode_steps=5,
+)
+
+
 del AntennaPlacementEnv
 try:
     from env.antennaEnv_V1_2 import AntennaPlacementEnv
@@ -89,11 +79,6 @@ except ImportError:
     AntennaPlacementEnv = None
     print("Custom Antenna Environment failed to import")
 
-register(
-    id="antenna3x3-v1_2",
-    entry_point=AntennaPlacementEnv,
-    max_episode_steps=5,
-)
 
 register(
     id="antenna3x3-v1.2_n",
@@ -101,27 +86,12 @@ register(
     max_episode_steps=1,
 )
 
-
-
-#### antenna3x4-v1.2
-del AntennaPlacementEnv
-try:
-    from env.antennaEnv_V1_2 import AntennaPlacementEnv
-except ImportError:
-    AntennaPlacementEnv = None
-    print("Custom Antenna Environment failed to import")
-
-register(
-    id="antenna3x4-v1.2",
-    entry_point=AntennaPlacementEnv,
-    max_episode_steps=25,
-)
-
 register(
     id="antenna3x4-v1.2_n",
     entry_point=create_normalized_env("antenna3x4-v1.2"),
     max_episode_steps=5,
 )
+
 
 #### end
     
