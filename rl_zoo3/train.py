0bb2115f275325e8fab40c78bab82419870a9151
yerkgb <104567963+yerkgb@users.noreply.github.com>
2025-08-16 09:04:10 +0300

    experiment after focused optimization with 100 trial and 20k timesteps.

--- rl_zoo3/train.py
+++ rl_zoo3/train.py
@@ -20,7 +20,7 @@ from rl_zoo3.utils import ALGOS, StoreDict
 
 def train() -> None:
     parser = argparse.ArgumentParser()
-    parser.add_argument("--exp-name", help="Experiment name", default=str("development2_trialNum_72"), type=str)
+    parser.add_argument("--exp-name", help="Experiment name", default=str("development2_focusOpt_trial12"), type=str)
     parser.add_argument("--algo", help="RL Algorithm", default="maskable_ppo", type=str, required=False, choices=list(ALGOS.keys()))
     parser.add_argument("--env", type=str, default="antenna3x4-v2.0", help="environment ID")
     parser.add_argument("--normalizeVec", help="ObsNormaliztion accross paralell envs", action="store_true", default=False)
